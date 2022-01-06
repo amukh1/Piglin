424cf074f2fccbf88075e0ed8db7ad7053a6ff24
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-01-06 13:02:51 -0500

    a

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function kills() {
     window.location.href = 'https://amukh1.github.io/Piglin/kills'
 }
+
+function dyingFrog() {
+    document.getElementById("piggy").style.left = "0px";
+    document.getElementById("boxx").style.left = "100px";
+    }
+    const myTimeoutt = setTimeout(dyingFrog, 700);
